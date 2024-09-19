1b4a6642a10e2b50d1f6bf1dc223cf0c51235a57
Giovanni-Damico98 <gd.installazioni25@gmail.com>
2024-09-19 20:06:57 +0200

    exercise complete + bonus

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppJumbotron from "./components/AppJumbotron.vue";
+import AppTitle from "./components/AppTitle.vue";
+
+AppTitle;
+export default {
+  components: { AppTitle, AppJumbotron },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <AppTitle />
+  <AppJumbotron />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h1>La mia prima app con Vue+Vite</h1>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: red;
+  font-size: 4rem;
+}
+</style>
